91abdc4171309c367096f8c8f4721461d781c8e3
dochi486 <93mskim@gmail.com>
2021-07-06 00:37:03 +0900

    좌우 이동 방향 바라보기, 이동
    키 눌렀다가 떼도 계속 움직이는 버그

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public float speed = 5f;
+    Vector2 move = Vector2.zero;
+    public GameObject playerSprite;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+            move.x = -1;
+        if (Input.GetKey(KeyCode.D))
+            move.x = 1;
+
+        if (move.sqrMagnitude > 0)
+            transform.Translate(speed * move * Time.deltaTime);
+
+        if (move.x >= 0)
+            playerSprite.transform.rotation = Quaternion.Euler(0, 180, 0);
+        else
+            playerSprite.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
