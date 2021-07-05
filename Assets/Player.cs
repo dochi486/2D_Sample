1d147b8922340127532e3c595acf147ae9b8d974
dochi486 <93mskim@gmail.com>
2021-07-06 00:40:19 +0900

    이동할 때 뛰는 애니메이션 추가

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,23 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 5f;
+    Animator animator;
     Vector2 move = Vector2.zero;
     public GameObject playerSprite;
     // Update is called once per frame
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
     void Update()
     {
+
         if (Input.GetKey(KeyCode.A))
             move.x = -1;
         if (Input.GetKey(KeyCode.D))
             move.x = 1;
 
         if (move.sqrMagnitude > 0)
+        {
             transform.Translate(speed * move * Time.deltaTime);
+            animator.Play("Run");
+        }
 
         if (move.x >= 0)
             playerSprite.transform.rotation = Quaternion.Euler(0, 180, 0);
