89e48923516e7929af035dd8c4e7bbb87985a1f4
dochi486 <93mskim@gmail.com>
2021-07-06 19:24:27 +0900

    Attack모션 추가, y축 이동 구현 중

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
     public float speed = 5f;
     Animator animator;
     Vector2 move = Vector2.zero;
     public GameObject playerSprite;
-    // Update is called once per frame
 
     private void Awake()
     {
@@ -15,21 +13,35 @@ public class Player : MonoBehaviour
     }
     void Update()
     {
+        Move();
 
-        if (Input.GetKey(KeyCode.A))
-            move.x = -1;
-        if (Input.GetKey(KeyCode.D))
-            move.x = 1;
-
-        if (move.sqrMagnitude > 0)
-        {
-            transform.Translate(speed  * move * Time.deltaTime);
-            animator.Play("Run");
-        }
+        if (Input.GetKeyDown(KeyCode.Space))
+            animator.Play("Attack"); //어택에서 자꾸 Idle로 가버리는 현상 -> 애니메이션 길이 구해서 끝나면 idle하도록
 
         if (move.x >= 0)
             playerSprite.transform.rotation = Quaternion.Euler(0, 180, 0);
         else
             playerSprite.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.A))
+            move.x = -1;
+        else if (Input.GetKey(KeyCode.D))
+            move.x = 1;
+        else if (Input.GetKey(KeyCode.W))
+            move.y = 1; //위로 이동해버리네~ 점프가 아니라~
+        else
+        {
+            animator.Play("Idle");
+            return;
+        }
+
+        if (move.sqrMagnitude > 0)
+        {
+            transform.Translate(speed * move * Time.deltaTime);
+            animator.Play("Run");
+        }
     }
 }
