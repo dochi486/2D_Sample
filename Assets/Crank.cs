84932912b540a4a88cc51d16135fb3a584260db3
dochi486 <93mskim@gmail.com>
2021-07-11 22:16:35 +0900

    Update Crank.cs

--- Assets/Crank.cs
+++ Assets/Crank.cs
@@ -8,11 +8,13 @@ public class Crank : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision == gameObject.CompareTag("Player"))
-        {
-            down.SetActive(true);
-            Debug.Log(collision.transform.name);
-            up.SetActive(false);
-        }
+        //콜리젼으로 up, down 껐다 키고 싶은데
+
+        //if (collision == gameObject.CompareTag("Player"))
+        //{
+        //    down.SetActive(true);
+        //    Debug.Log(collision.transform.name);
+        //    up.SetActive(false);
+        //}
     }
 }
