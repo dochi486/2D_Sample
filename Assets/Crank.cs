53530a1c16c65cce4d3bc2756bfb49bcb235fa02
dochi486 <93mskim@gmail.com>
2021-07-11 21:44:59 +0900

    asset추가, trigger로 스위치 작동하게 할 예정

---
+++ Assets/Crank.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Crank : MonoBehaviour
+{
+
+    public GameObject down;
+    public GameObject up;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == gameObject.CompareTag("Player"))
+        {
+            down.SetActive(true);
+            Debug.Log(collision.transform.name);
+            up.SetActive(false);
+        }
+    }
+}
