6e9b1f26eadc623dce3e129af52c13665ee921fd
dochi486 <93mskim@gmail.com>
2021-07-11 23:09:25 +0900

    씬에 몹 배치, start할 때 disable
    crank로 나타나게 할 예정

---
+++ Assets/Monster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public GameObject crankDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (crankDown.gameObject == isActiveAndEnabled)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
