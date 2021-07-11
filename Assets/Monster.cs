9753433bdf2e4469baf229421eb6f20ed38f6ec1
dochi486 <93mskim@gmail.com>
2021-07-11 23:40:51 +0900

    트랩 밟았을 때 몹 등장

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     public GameObject crankDown;
+    SpriteRenderer beeSprite;
 
+    private void Awake()
+    {
+        beeSprite = GetComponentInChildren<SpriteRenderer>();
+        
+    }
+
+    private void Start()
+    {
+        beeSprite.gameObject.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (crankDown.activeInHierarchy == true)
         {
+            beeSprite.gameObject.SetActive(true);
             //sprite.SetActive(true);
-            print("Trap Activated");
+            //print("Trap Activated");
         }
     }
 }
