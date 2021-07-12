7ec29a5855bb78ee33e43d2d2255c4527e2fb3f5
dochi486 <93mskim@gmail.com>
2021-07-13 00:49:12 +0900

    상자 열릴 때 보석 애니메이션 재생

--- Assets/Gem.cs
+++ Assets/Gem.cs
@@ -1,20 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Gem : MonoBehaviour
 {
     public GameObject chest;
     SpriteRenderer gem;
+    Animator animator;
 
     private void Start()
     {
         gem = GetComponentInChildren<SpriteRenderer>();
         gem.enabled = false;
+        animator = GetComponentInChildren<Animator>();
     }
     private void Update()
     {
         if (chest.activeInHierarchy == true)
+        {
             gem.enabled = true;
+            animator.Play("gem");
+        }
     }
 }
