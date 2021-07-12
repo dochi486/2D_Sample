59ed39f2452b8454a1d5950b2ead51ea12337ecc
dochi486 <93mskim@gmail.com>
2021-07-13 01:06:22 +0900

    별 애니메이션 추가, 플레이어 상호작용 추가

---
+++ Assets/Star.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    Animator animator;
+    SpriteRenderer star;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        star = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(StarFadeCo());
+        }
+    }
+    public float animationTime = 0.55f;
+
+    IEnumerator StarFadeCo()
+    {
+        animator.Play("Star_fade");
+
+        yield return new WaitForSeconds(animationTime);
+
+        star.enabled = false;
+    }
+}
