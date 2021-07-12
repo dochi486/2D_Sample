9bdf3d19c19fcca6d0a080aba18c6a2f10e4cd2f
dochi486 <93mskim@gmail.com>
2021-07-12 18:52:07 +0900

    hp5일 때 UI 표시

--- Assets/EventManager.cs
+++ Assets/EventManager.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class EventManager : MonoBehaviour
 {
+    public Image hpStatus5;
+    Player player;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.hp == 5)
+            hpStatus5.enabled = true;
     }
 }
