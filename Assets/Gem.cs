75ddb2424fcde56980c10436e1907e6c29c0e926
dochi486 <93mskim@gmail.com>
2021-07-13 00:40:46 +0900

    상자 열리면 보석 나오게 했따

---
+++ Assets/Gem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    public GameObject chest;
+    SpriteRenderer gem;
+
+    private void Start()
+    {
+        gem = GetComponentInChildren<SpriteRenderer>();
+        gem.enabled = false;
+    }
+    private void Update()
+    {
+        if (chest.activeInHierarchy == true)
+            gem.enabled = true;
+    }
+}
