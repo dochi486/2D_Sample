9094d7c52922ebf916f32aef63e342ebb3f76afa
dochi486 <93mskim@gmail.com>
2021-07-13 13:25:31 +0900

    필요없는 주석 삭제
    스위치를 다시 작동시키는 건 의미가 없는 것 같아

--- Assets/Crank.cs
+++ Assets/Crank.cs
@@ -9,17 +9,11 @@ public class Crank : MonoBehaviour
     public bool isTrapActivated = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //print(collision.transform.name);
         if (collision.CompareTag("Player"))
         {
             isTrapActivated = true;
             down.gameObject.SetActive(true);
             up.gameObject.SetActive(false);
         }  
-        //else if (collision.CompareTag("Player"))
-        //{
-        //    down.gameObject.SetActive(false);
-        //    up.gameObject.SetActive(true);
-        //}
     }
 }
