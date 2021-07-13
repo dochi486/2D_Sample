e77e5773731290a0687c6b7af1e64ee594966672
dochi486 <93mskim@gmail.com>
2021-07-13 12:39:07 +0900

    표지판 문구 코루틴으로 제어
    애니메이션으로 페이드아웃까지

--- Assets/Sign.cs
+++ Assets/Sign.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,13 +7,37 @@ public class Sign : MonoBehaviour
 {
     public Image sign;
     public TextMeshProUGUI text;
+    Animator animatorSign;
+    Animator animatorText;
+
+    private void Awake()
+    {
+        animatorSign = sign.GetComponent<Animator>();
+        animatorText = text.GetComponent<Animator>();
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            sign.gameObject.SetActive(true);
-            text.gameObject.SetActive(true);
+            StartCoroutine(SignCo());
         }
+    }
+    public float animationTime = 0.6f;
+    IEnumerator SignCo()
+    {
+        sign.gameObject.SetActive(true);
+        text.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(5);
+        animatorText.Play("TextFadeOut");
+        animatorSign.Play("SignTextBoxFadeOut");
+
+        yield return new WaitForSeconds(animationTime);
+
+        sign.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
+
     }
 }
