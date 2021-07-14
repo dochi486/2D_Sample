018027b3e1c1e6f1539cad791e7700a8cd369faf
dochi486 <93mskim@gmail.com>
2021-07-15 07:58:03 +0900

    리팩토링, 플레이어 스프라이트 수정
    1. Chest, Crank 같이 플레이어와 상호작용하는 오브젝트 상속으로 사용하도록 리팩토링
    2. 플레이어 스프라이트에 아웃라인 쉐이더 추가해서 좀 더 다른 오브젝트랑 분위기 맞도록 수정

---
+++ Assets/Scripts/InteractiveObjects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObjects : MonoBehaviour
+{
+    public GameObject objectOff; //플레이어와 닿기 전에 켜져있는 오브젝트
+    public GameObject objectOn; //플레이어와 충돌하면 켜지는 오브젝트
+
+    public bool isObjectActivated = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            isObjectActivated = true;
+            objectOff.gameObject.SetActive(false);
+            objectOn.gameObject.SetActive(true);
+        }
+    }
+}
