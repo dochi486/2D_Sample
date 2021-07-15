d21f8016cd7825389a582a5659c9bb3b86394945
dochi486 <93mskim@gmail.com>
2021-07-15 15:12:00 +0900

    게임오버씬
    DOFade로 페이드인 되도록 수업시간에 썼던 코드 재사용

---
+++ Assets/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    CanvasGroup blackScreen;
+
+    private void Start()
+    {
+        blackScreen = GameObject.Find("PersistCanvas").transform.Find("BlackScreen").GetComponent<CanvasGroup>();
+        blackScreen.gameObject.SetActive(true);
+        blackScreen.alpha = 1;
+        blackScreen.DOFade(0, 1.5f).OnComplete(() => blackScreen.gameObject.SetActive(false));
+    }
+
+}
