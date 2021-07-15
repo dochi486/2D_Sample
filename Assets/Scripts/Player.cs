bca0f6188a01ca2ea65cf5e50f45ecd08c607314
dochi486 <93mskim@gmail.com>
2021-07-15 09:52:19 +0900

    플레이어 수정
    1. 스프라이트 아웃라인 좀 더 얇게 수정
    2. 점프 2D런에서 하던 것 가져옴. 상태 변화 부분 수정해야한다. 코루틴 쓰면 될 것도 같은데

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,8 @@ public class Player : MonoBehaviour
     public int hp = 4;
     public static Player instance;
     public PlayerState state = PlayerState.Idle;
+    Rigidbody2D rigid;
+    public Vector2 jumpForce = new Vector2(0, 500);
     PlayerState State
     {
         get { return state; }
@@ -38,6 +40,7 @@ public class Player : MonoBehaviour
     {
         instance = this;
         animator = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
@@ -62,38 +65,44 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            StartCoroutine(JumpCo());
+            State = PlayerState.Jump;
+            //StartCoroutine(JumpCo());
+            rigid.velocity = Vector2.zero;
+            rigid.AddForce(jumpForce);
+            //점프가 끝나면 Idle하도록 코루틴 ? 
+            //지금은 떨어지면 아이들이랑 런 겹치고 있다.. 
+            State = PlayerState.Idle;
         }
     }
 
-    public AnimationCurve jumpYac;
-    public float jumpYMultiply = 20;
-    public float jumpTimeMultiply = 1;
+    //public AnimationCurve jumpYac;
+    //public float jumpYMultiply = 20;
+    //public float jumpTimeMultiply = 1;
 
-    private IEnumerator JumpCo()
-    {
-        State = PlayerState.Jump;
-        float jumpStartTime = Time.time;
-        float jumpDuration = jumpYac[jumpYac.length - 1].time;
-        jumpDuration *= jumpTimeMultiply;
-        float jumpEndTime = jumpStartTime + jumpDuration;
-        float sumEvaluateTime = 0;
+    //private IEnumerator JumpCo()
+    //{
+    //    State = PlayerState.Jump;
+    //    float jumpStartTime = Time.time;
+    //    float jumpDuration = jumpYac[jumpYac.length - 1].time;
+    //    jumpDuration *= jumpTimeMultiply;
+    //    float jumpEndTime = jumpStartTime + jumpDuration;
+    //    float sumEvaluateTime = 0;
 
-        float jumpStartY = transform.position.y;
-        while (Time.time < jumpEndTime)
-        {
-            float y = jumpYac.Evaluate(sumEvaluateTime / jumpTimeMultiply);
-            y *= jumpYMultiply;
-            float currentY = jumpStartY + y;
-            var trPos = transform.position;
-            trPos.y = currentY;
-            transform.position = trPos;
-            yield return null;
-            sumEvaluateTime += Time.deltaTime;
-        }
+    //    float jumpStartY = transform.position.y;
+    //    while (Time.time < jumpEndTime)
+    //    {
+    //        float y = jumpYac.Evaluate(sumEvaluateTime / jumpTimeMultiply);
+    //        y *= jumpYMultiply;
+    //        float currentY = jumpStartY + y;
+    //        var trPos = transform.position;
+    //        trPos.y = currentY;
+    //        transform.position = trPos;
+    //        yield return null;
+    //        sumEvaluateTime += Time.deltaTime;
+    //    }
 
-        State = PlayerState.Idle;
-    }
+    //    State = PlayerState.Idle;
+    //}
 
     private void Move()
     {
