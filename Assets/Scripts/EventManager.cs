1b25523104fb034de5b4b87509a9746028a4cfcd
dochi486 <93mskim@gmail.com>
2021-07-18 02:59:29 +0900

    hp에 따른 ui변화 for문으로 만들기 실패
    울고싶다

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,42 +8,59 @@ public class EventManager : MonoBehaviour
 
 
     public string loadSceneName;
+    public int maxHealth = 4;
     void Start()
     {
         healthImages = GetComponentsInChildren<Image>();
-        healthImages[1].enabled = false;
-        healthImages[2].enabled = false;
-        healthImages[3].enabled = false;
+        //healthImages[1].enabled = false;
+        //healthImages[2].enabled = false;
+        //healthImages[3].enabled = false;
+
     }
 
     void Update()
     {
-
-        switch (Player.instance.hp)
+        for (int i = 0; i < maxHealth; i++)
         {
-            case 4:
-                healthImages[0].enabled = true;
-                break;
-            case 3:
-                healthImages[0].enabled = false;
-                healthImages[1].enabled = true;
-                break;
-            case 2:
-                healthImages[2].enabled = true;
-                healthImages[0].enabled = false;
-                healthImages[1].enabled = false;
-                break;
-            case 1:
-                healthImages[3].enabled = true;
-                healthImages[0].enabled = false;
-                healthImages[1].enabled = false;
-                healthImages[2].enabled = false;
-                break;
-            case 0:
+            if (Player.instance.hp <= maxHealth)
+            {
+                healthImages[i].enabled = true;
+            }
+            else if (Player.instance.hp == 0)
+            {
                 StartCoroutine(GameOverCo());
                 UnityEngine.SceneManagement.SceneManager.LoadScene(loadSceneName);
-                break;
+            }
+            else
+            {
+                healthImages[i].enabled = false;
+            }
         }
+        //switch (Player.instance.hp)
+        //{
+        //    case 4:
+        //        healthImages[0].enabled = true;
+        //        break;
+        //    case 3:
+        //        healthImages[0].enabled = false;
+        //        healthImages[1].enabled = true;
+        //        break;
+        //    case 2:
+        //        healthImages[2].enabled = true;
+        //        healthImages[0].enabled = false;
+        //        healthImages[1].enabled = false;
+        //        break;
+        //    case 1:
+        //        healthImages[3].enabled = true;
+        //        healthImages[0].enabled = false;
+        //        healthImages[1].enabled = false;
+        //        healthImages[2].enabled = false;
+        //        break;
+        //    case 0:
+        //        StartCoroutine(GameOverCo());
+        //        UnityEngine.SceneManagement.SceneManager.LoadScene(loadSceneName);
+        //        break;
+        //}
 
     }
     public float gameEndTime = 5;
