0bfc830dbef559ec4515c53fe99799cd59f7fdad
dochi486 <93mskim@gmail.com>
2021-07-24 17:00:32 +0900

    에디터에서 씬 불러오는 메뉴 생성, 타이틀 폰트 변경

---
+++ Assets/Scripts/EditorSceneLoad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+using System;
+
+public class EditorSceneLoad
+{
+    [MenuItem("Window/1. Title Scene")]
+    public static void TitleSceneLoad()
+    {
+        LoadScene("Title");
+    }
+
+    [MenuItem("Window/2. Main Scene")]
+    public static void MainSceneLoad()
+    {
+        LoadScene("Main");
+    }
+
+    private static void LoadScene(string loadSceneName)
+    {
+        EditorSceneManager.OpenScene($"Assets/Scenes/{loadSceneName}.unity");
+    }
+}
