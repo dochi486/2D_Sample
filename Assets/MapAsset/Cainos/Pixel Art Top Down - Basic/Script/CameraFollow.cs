7ce2da3f4eb1f607fccdef9a98b454540e5b5790
dochi486 <93mskim@gmail.com>
2021-08-15 00:48:12 +0900

    카메라 트랜스폼 포지션의 y값 고정해서 배경 벗어나지 않게 수정

--- Assets/MapAsset/Cainos/Pixel Art Top Down - Basic/Script/CameraFollow.cs
+++ Assets/MapAsset/Cainos/Pixel Art Top Down - Basic/Script/CameraFollow.cs
@@ -10,8 +10,6 @@ namespace Cainos.PixelArtTopDown_Basic
         public Transform target;
         public Vector3 offset; //offset도 필요 없을 것 같긴한데
 
-        public BoxCollider2D moveableArea;
-
         [SerializeField]float minY, maxY;
 
         
@@ -23,29 +21,12 @@ namespace Cainos.PixelArtTopDown_Basic
             target = Player.instance.transform;
 
             offset = target.position - transform.position;
-
-            //카메라의 포지션 y가 박스컬라이더 범위 밖으로 나가지 않게
-
-            minY = height / 2 + moveableArea.transform.position.z + moveableArea.size.y - moveableArea.size.y / 2;
-            maxY = -height / 2 + moveableArea.transform.position.z + moveableArea.size.y + moveableArea.size.y / 2;
-
-            //카메라 포지션 x는 음수 양수 이동 가능
-
-
-
         }
-
-        public float lerp = 0.05f;
         void Update()
         {
-
-            var newPos = target.position - offset;
-
-            newPos.y = Mathf.Min(newPos.y, maxY);
-            newPos.y = Mathf.Max(newPos.y, minY);
-
-            transform.position = Vector3.Lerp(transform.position, newPos, lerp);
-
+            var cameraPosY = GetComponent<Camera>().transform.position;
+            cameraPosY.y = 0;
+            transform.position = cameraPosY;
         }
     }
 }
