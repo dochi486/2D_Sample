5dd47c240344bd028c3379cb67c160483f74ff88
dochi486 <93mskim@gmail.com>
2021-08-15 13:12:28 +0900

    카메라 수정 필요

--- Assets/MapAsset/Cainos/Pixel Art Top Down - Basic/Script/CameraFollow.cs
+++ Assets/MapAsset/Cainos/Pixel Art Top Down - Basic/Script/CameraFollow.cs
@@ -10,6 +10,8 @@ namespace Cainos.PixelArtTopDown_Basic
         public Transform target;
         public Vector3 offset; //offset도 필요 없을 것 같긴한데
         
+        //todo: 카메라 y값은 고정되어 있고 플레이어를 따라서 x축으로만 움직이도록 수정
+        //->지금은 아예 고정 상태..
         void Start()
         {
             var camera = GetComponent<Camera>();
